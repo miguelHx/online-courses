33329a90f7165e87ac58053f3bb5c6a3684abeb0
Miguel <mhernandez401405@gmail.com>
2026-03-26 19:58:44 -0700

    update 1b soln

--- courses/intro-to-cs-using-python-mit-6.001L/1_ps1/ps1b.py
+++ courses/intro-to-cs-using-python-mit-6.001L/1_ps1/ps1b.py
@@ -7,36 +7,40 @@
 ## Get user input for yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise below ##
 ##########################################################################################
 
+yearly_salary = float(input('enter yearly salary: '))
+portion_saved = float(input('enter % of salary to save, as a decimal: '))
+cost_of_dream_home = float(input('enter cost of dream home: '))
+semi_annual_raise = float(input('enter semi-annual raise, as a decimal: '))
+
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
+
+portion_down_payment = 0.25
+amount_saved = 0
+r = 0.05
+
+monthly_rate = r / 12
+monthly_saved = yearly_salary / 12 * portion_saved
+
+# monthly_return = amount_saved * (r/12)
+
+cost_of_down_payment = cost_of_dream_home * portion_down_payment
 
 
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
 
-
-yearly_salary = float(input('enter yearly salary: '))
-portion_saved = float(input('enter % of salary to save, as a decimal: '))
-cost_of_dream_home = float(input('enter cost of dream home: '))
-semi_annual_raise = float(input('enter semi-annual raise, as a decimal: '))
-
-portion_down_payment = 0.25
-r = 0.05
-down_payment_amount = cost_of_dream_home * portion_down_payment
-
-first_month_saved = (yearly_salary / 12.0) * portion_saved
-amt_saved = 0
-monthly_return = first_month_saved * (r / 12)
-
 months = 0
-while amt_saved <= down_payment_amount:
-    amt_saved += first_month_saved + monthly_return
-    monthly_return = amt_saved * (r / 12)
+
+while amount_saved < cost_of_down_payment:
+    monthly_return = amount_saved * monthly_rate
+    amount_saved = amount_saved + monthly_saved + monthly_return
     months += 1
     if months % 6 == 0:
         yearly_salary += yearly_salary * semi_annual_raise
-        first_month_saved = (yearly_salary / 12.0) * portion_saved
+        monthly_saved = (yearly_salary / 12.0) * portion_saved
+
 print('number of months: ', months)
